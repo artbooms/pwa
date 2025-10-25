09f32cd4ebf619a15b99e864a15f969555229968
artbooms <info@artbooms.com>
2025-10-26 01:05:49 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+// Minimal service worker for Artbooms PWA
+self.addEventListener('install', (event) => { self.skipWaiting(); });
+self.addEventListener('activate', (event) => { event.waitUntil(self.clients.claim()); });
+self.addEventListener('fetch', (event) => { event.respondWith(fetch(event.request)); });
